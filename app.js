32149487812eb9b75ce1207c93653fe50c5ef6f6
Hezrondavid <hezrondavid946@gmail.com>
2025-10-04 00:32:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 module.exports = app;
