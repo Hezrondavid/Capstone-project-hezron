c38723f3a8cc46d1f1853f32617f20d2a521738f
Hezrondavid <hezrondavid946@gmail.com>
2025-10-03 22:43:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Node app is running at http://0.0.0.0:${PORT}`);
-});
+// Export the app for testing
+module.exports = app;
+
+// Start the server only if this file is run directly
+if (require.main === module) {
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Node app is running at http://0.0.0.0:${PORT}`);
+  });
+}
