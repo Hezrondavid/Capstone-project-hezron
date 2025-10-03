278fdd1579cb0de0980be9839ee7ac3864ea58c5
Hezrondavid <hezrondavid946@gmail.com>
2025-10-03 23:17:36 +0530

    Update index.js with new content

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-const express = require('express');
-const app = express();
+const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Node app is running at http://0.0.0.0:${PORT}`);
 });
-
-// Export the app for testing
-module.exports = app;
-
-// Start the server only if this file is run directly
-if (require.main === module) {
-  app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Node app is running at http://0.0.0.0:${PORT}`);
-  });
-}
